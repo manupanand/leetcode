1e844c8d160dee1ef8ab7224089e9a9b4506d35b
manupanand <manupanandindianinvestor@gmail.com>
2024-07-12 10:18:19 +0530

    new codes

---
+++ binarycount/binarycount.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int sumOfBinary(int numValue){
+    int sizeofArray=numValue+1;
+    int arr[sizeofArray];
+    for(int i=0;i<=sizeofArray;i++){
+
+    }
+
+return arr;
+}
+int makeBinary(int value){
+    int arry[value+1];
+    if (value==0){
+        arry[0]={0};
+    } return ;
+    if (value==0) return arry[1];
+}
+int main(){
+
+    return 0;
+}
