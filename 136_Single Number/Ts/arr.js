5dc5fb8ef35b6500ded42df83a4f35259f5df6be
manupanand <manupanandindianinvestor@gmail.com>
2024-07-12 22:52:05 +0530

    new question

---
+++ 136_Single Number/Ts/arr.js
@@ -0,0 +1,9 @@
+"use strict";
+function singleNumber(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        return nums[5];
+    }
+    return nums[5];
+}
+;
+console.log(singleNumber([1, 2, 2, 3, 3, 4, 4]));
---
+++ 136_Single Number/Ts/arr.ts
@@ -0,0 +1,8 @@
+function singleNumber(nums: number[]): number {
+    arr:number[]=[0]
+    for(let i=0;i<nums.length;i++){
+      return nums[5];
+    }
+    return nums[5];
+};
+console.log(singleNumber([1,2,2,3,3,4,4]))
